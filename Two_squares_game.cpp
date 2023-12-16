a1fd47730c263ac9757c8cf9fe0e539ca184e5ed
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-12-16 19:13:29 +0200

    Update Two_squares_game.cpp

--- Two_squares_game.cpp
+++ Two_squares_game.cpp
@@ -1,7 +1,6 @@
 #include "Two_squares_game.h"
 using namespace std;
 
-
 //___________________________________________________________________________________
 // function to determine the winner by comparing each two blocks beside each other are filled or not
 bool checkWinner(int table[5][5])
@@ -23,7 +22,7 @@ bool checkWinner(int table[5][5])
 
 //___________________________________________________________________________________
 // function to find the block the user want by the number written on it
-void find(int& x, int& y, int att, int table[5][5])
+void find(int &x, int &y, int att, int table[5][5])
 {
     for (int i = 0; i < 4; i++)
     {
@@ -54,10 +53,14 @@ void playerTurn(int conter)
 //  function that print the board
 void printBoard(int table[5][5])
 {
-    cout << setw(3) << table[0][0] << " | " << setw(3) << table[0][1] << " | " << setw(3) << table[0][2] << " | " << setw(3) << table[0][3] << endl;
-    cout << setw(3) << table[1][0] << " | " << setw(3) << table[1][1] << " | " << setw(3) << table[1][2] << " | " << setw(3) << table[1][3] << endl;
-    cout << setw(3) << table[2][0] << " | " << setw(3) << table[2][1] << " | " << setw(3) << table[2][2] << " | " << setw(3) << table[2][3] << endl;
-    cout << setw(3) << table[3][0] << " | " << setw(3) << table[3][1] << " | " << setw(3) << table[3][2] << " | " << setw(3) << table[3][3] << endl;
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            cout << setw(3) << table[i][j] << " | ";
+        }
+        cout << setw(3) << table[i][3] << endl;
+    }
 }
 //___________________________________________________________________________________
 void checkSquares(int att1, int att2, int x, int x2, int y, int y2, int counter, int table[5][5])
